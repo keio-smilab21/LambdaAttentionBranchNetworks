8b3950568b7936c3a03cc828553e0f3f3073a4f0
Tsumugi IIDA <tiida@keio.jp>
2022-03-15 16:15:23 +0900

    [change] use batchsampler by default

--- data/__init__.py
+++ data/__init__.py
@@ -177,7 +177,7 @@ def get_parameter_depend_in_data_set(
     params["root"] = dataset_root
     params["has_params"] = False
     params["num_channel"] = 3
-    params["sampler"] = False
+    params["sampler"] = True
     # ImageNet
     params["mean"] = (0.485, 0.456, 0.406)
     params["std"] = (0.229, 0.224, 0.225)
