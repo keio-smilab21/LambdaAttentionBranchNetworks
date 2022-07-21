[fix] no change patch_size when kl-random

utils/mask_generator.py
@@ -1,4 +1,5 @@
 import math
+from tempfile import tempdir
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,6 +18,7 @@ WEIGHT = [0.2, 0.5, 0.2, 0.1]
 # PATCH_CHOICE = [1, 4, 8, 16, 32]
 # WEIGHT_PATCH = [0.7, 0.2, 0.05, 0.03, 0.02]
 PATCH_CHOICE = [1, 4]
+STEP = [500, 20]
 WEIGHT_PATCH = [0.7, 0.3]
 
 class Mask_Generator():
@@ -74,25 +76,25 @@ class Mask_Generator():
             mask_input = self.create_one_mask_image(idx=i)
             mask_inputs[i] = mask_input
         
-        if self.save_mask_input:
-            # mask 画像の保存
-            # img = mask_inputs[-1].reshape(self.images.shape[2], self.images.shape[3]) # (512, 512)
-            print("aaa", mask_inputs[-1].shape)
-            # img = np.transpose(mask_inputs[-1], (1,2,0))
-            params = get_parameter_depend_in_data_set("IDRiD")
-            img = reverse_normalize(img, params["mean"], params["std"])
-            img = np.transpose(mask_inputs[-1], (1,2,0))
-            img = (img * 255).astype(np.uint8)
-            # fig, ax = plt.subplots()
-            # if img.shape[-1] == 1:
-            #     im = ax.imshow(img, cmap="gray")
-            # else:
-            #     im = ax.imshow(img, cmap="gray")
-            # fig.colorbar(im)
-            # plt.savefig(f"mask_image/deletion/mask_ratio_{self.ratio}_{self.data_name}.png")
-            # plt.clf()
-            # plt.close()
-            Image.fromarray(img).save(f"mask_image/deletion/mask_ratio_{self.ratio}_{self.data_name}.png")
+        # if self.save_mask_input:
+        #     # mask 画像の保存
+        #     # img = mask_inputs[-1].reshape(self.images.shape[2], self.images.shape[3]) # (512, 512)
+        #     print("aaa", mask_inputs[-1].shape)
+        #     # img = np.transpose(mask_inputs[-1], (1,2,0))
+        #     params = get_parameter_depend_in_data_set("IDRiD")
+        #     img = reverse_normalize(img, params["mean"], params["std"])
+        #     img = np.transpose(mask_inputs[-1], (1,2,0))
+        #     img = (img * 255).astype(np.uint8)
+        #     # fig, ax = plt.subplots()
+        #     # if img.shape[-1] == 1:
+        #     #     im = ax.imshow(img, cmap="gray")
+        #     # else:
+        #     #     im = ax.imshow(img, cmap="gray")
+        #     # fig.colorbar(im)
+        #     # plt.savefig(f"mask_image/deletion/mask_ratio_{self.ratio}_{self.data_name}.png")
+        #     # plt.clf()
+        #     # plt.close()
+        #     Image.fromarray(img).save(f"mask_image/deletion/mask_ratio_{self.ratio}_{self.data_name}.png")
 
 
         return mask_inputs # B, C, H, W
@@ -171,7 +173,13 @@ class Mask_Generator():
                     mask_img[c] = (image[c] * mask - mean[c]) / std[c]
                 elif self.mask_mode == "mean":
                     mask_img[c] = mask * ((image[c] - mean[c]) / std[c])
-
+        
+        # if self.patch_size == 1:
+        #     print("save patch image")
+        #     import matplotlib.pyplot as plt
+        #     temp = temp.transpose(1,2,0)
+        #     plt.imshow(temp)
+        #     plt.savefig("testtest1111.jpg")
         return mask_img
 
     def divide_attention_map_into_patch(self, attention):
@@ -179,6 +187,9 @@ class Mask_Generator():
 
         if self.KL_mask_random:
             p_size = np.random.choice(PATCH_CHOICE, p=WEIGHT_PATCH)
+            self.patch_size = p_size
+            idx = PATCH_CHOICE.index(p_size)
+            self.step = STEP[idx]
         else:
             p_size = self.patch_size
         return skimage.measure.block_reduce(attention, (p_size, p_size), np.max)